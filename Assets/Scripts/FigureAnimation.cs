b23cb07cccc24bbcb1ed4997227e0b9938cf5044
cryscan <cryscan@umich.edu>
2020-10-11 18:28:14 -0400

    Add hands to characters.

--- Assets/Scripts/FigureAnimation.cs
+++ Assets/Scripts/FigureAnimation.cs
@@ -1,25 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class FigureAnimation : MonoBehaviour
 {
-    [SerializeField] GameObject head, body;
+    [SerializeField] Transform body;
+    [SerializeField] Transform[] attaches;
 
     [SerializeField] float period = 0.5f;
     [SerializeField] float verticalAmplitude = 0.2f;
 
-    [SerializeField] float headLag = Mathf.PI / 8;
+    [SerializeField] float attachLag = Mathf.PI / 8;
 
     [Range(0, 1)]
-    public float blend = 1;
+    [SerializeField] float targetBlend = 0;
+    [SerializeField] float blendFallout = 5;
 
-    float headHeight, bodyHeight;
+    float bodyHeight;
+    float[] attachHeights;
+    float blend;
 
     void Awake()
     {
-        headHeight = head.transform.position.y;
         bodyHeight = body.transform.position.y;
+        attachHeights = attaches.Select(_transform => _transform.position.y).ToArray();
+    }
+
+    void Update()
+    {
+        blend = Mathf.Lerp(blend, targetBlend, 1 - Mathf.Exp(-blendFallout * Time.deltaTime));
     }
 
     void LateUpdate()
@@ -27,13 +37,21 @@ public class FigureAnimation : MonoBehaviour
         float frequency = 2 * Mathf.PI / period;
 
         var _bodyHeight = Mathf.Lerp(bodyHeight, bodyHeight + verticalAmplitude / 2 * (Mathf.Sin(frequency * Time.time) + 1), blend);
-        var position = body.transform.position;
+        var position = body.position;
         position.y = _bodyHeight;
-        body.transform.position = position;
+        body.position = position;
 
-        var _headHeight = Mathf.Lerp(headHeight, headHeight + verticalAmplitude / 2 * (Mathf.Sin(frequency * Time.time + headLag) + 1), blend);
-        position = head.transform.position;
-        position.y = _headHeight;
-        head.transform.position = position;
+        for (int i = 0; i < attaches.Length; ++i)
+        {
+            var _transform = attaches[i];
+            var height = attachHeights[i];
+
+            var _height = Mathf.Lerp(height, height + verticalAmplitude / 2 * (Mathf.Sin(frequency * Time.time + attachLag) + 1), blend);
+            position = _transform.transform.position;
+            position.y = _height;
+            _transform.position = position;
+        }
     }
+
+    public void SetTargetBlend(float blend) => targetBlend = blend;
 }
