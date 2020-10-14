3c87679743ba8c0bdc18bc6a36748dd9169b450c
cryscan <cryscan@umich.edu>
2020-10-13 21:45:05 -0400

    Rebake lighting.

---
+++ Assets/Scripts/Memory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Memory : MonoBehaviour
+{
+    [SerializeField] List<Collectable> collectables;
+    Dictionary<Collectable, Vector3> collectablePositions;
+
+    void Awake()
+    {
+        foreach (var collectable in collectables)
+            collectablePositions.Add(collectable, collectable.transform.position);
+    }
+
+    public bool GetCollectablePosition(Collectable collectable, out Vector3 position)
+    {
+        if (collectablePositions.ContainsKey(collectable))
+        {
+            position = collectablePositions[collectable];
+            return true;
+        }
+
+        position = Vector3.zero;
+        return false;
+    }
+
+    public void SetCollectablePosition(Collectable collectable, Vector3 position) => collectablePositions[collectable] = position;
+}
